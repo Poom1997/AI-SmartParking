2cde307cdb33db0ddc0e31ee4eb29cfe248d75d9
iceyokuna <iceyokuna@hotmail.com>
2017-12-11 18:56:33 +0700

    Fixed System interface
    - Graph Manager can handle Node from 0 - N
    (Old version 1 - N)

--- GraphManager.py
+++ GraphManager.py
@@ -4,14 +4,14 @@ class GraphManager:
     def __init__(self):
         self.graph = Graph()
 
-    def setGraph(self, mat):
+    def setGraph(self, mat , goal):
         #always update the map when modify the map.
-        self.graph.setGraph(mat)
+        self.graph.setGraph(mat, [x + 1 for x in goal])
 
-    def getPath(self, A, B):
+    def getPath(self, A, B , status):
         #return list of path from start node to destination
-        path_list = self.graph.getPath(A , B)
-        return path_list
+        path_list = self.graph.getPath(A , B, status)
+        return [x - 1 for x in path_list]
 
     def printNode(self):
         self.graph.printAllNode()
