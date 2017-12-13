1183e7e405d2771cdfd979a59898d37f73000ea9
iceyokuna <iceyokuna@hotmail.com>
2017-12-13 14:43:55 +0700

    Fix utility.pl conflict

--- utility.pl
+++ utility.pl
@@ -1,4 +1,4 @@
-% Park node for optimal.
+% Park node for optimal
 
 parknode(PList,Result) :- parkmin(PList,999,n0,Result).
 parkmin([],_,Result,Result) :- !.
@@ -13,7 +13,7 @@ parkmin([H|T],Min,Minnode,Result) :-  findall(Y,park(H,Y),TList),
 					parkmin(T,Min,Minnode,Result).
 
 
-% Exit node for optimal.
+% Exit node for optimal
 exitnode(PList,Result) :- exitmin(PList,999,n0,Result).
 exitmin([],_,Result,Result) :- !.
 exitmin([H|T],Min,Minnode,Result) :-  findall(Y,exit(H,Y),TList),
