06e3a98c61a67e95cd0e857614ef3eda7701e2f0
bhurinuthw <poom1997@gmail.com>
2017-12-14 22:27:28 +0700

    remove comments

--- heuristic.pl
+++ heuristic.pl
@@ -1,7 +1,4 @@
-% heuristic(Vertex0, Ss) is true if Ss is a list of structures s(Vertex, Dist,
-%   Path) containing the shortest Path from Vertex0 to Vertex, the distance of
-%   the path being Dist.  The graph is defined by e/3.
-% e.g. heuristic(penzance, Ss)
+
 heuristic(Vertex, Ss):-
   create(Vertex, [Vertex], Ds),
   heuristic_1(Ds, [s(Vertex,0,[])], Ss).
@@ -19,25 +16,14 @@ heuristic_1([D|Ds], Ss0, Ss):-
   merge(Ds1, Ds4, Ds5),
   heuristic_1(Ds5, Ss1, Ss).
 
-% path(Vertex0, Vertex, Path, Dist) is true if Path is the shortest path from
-%   Vertex0 to Vertex, and the length of the path is Dist. The graph is defined
-%   by e/3.
-% e.g. path(penzance, london, Path, Dist)
 path(Vertex0, Vertex, Path, Dist):-
   heuristic(Vertex0, Ss),
   member(s(Vertex,Dist,Path), Ss), !.
   
-% create(Start, Path, Edges) is true if Edges is a list of structures s(Vertex,
-%   Distance, Path) containing, for each Vertex accessible from Start, the
-%   Distance from the Vertex and the specified Path.  The list is sorted by the
-%   name of the Vertex.
 create(Start, Path, Edges):-
   setof(s(Vertex,Edge,Path), e(Start,Vertex,Edge), Edges), !.
 create(_, _, []).
   
-% best(Edges, Edge0, Edge) is true if Edge is the element of Edges, a list of
-%   structures s(Vertex, Distance, Path), having the smallest Distance.  Edge0
-%   constitutes an upper bound.
 best([], Best, Best).
 best([Edge|Edges], Best0, Best):-
   shorter(Edge, Best0), !,
@@ -47,9 +33,6 @@ best([_|Edges], Best0, Best):-
 
 shorter(s(_,X,_), s(_,Y,_)):-X < Y.
 
-% delete(Xs, Ys, Zs) is true if Xs, Ys and Zs are lists of structures s(Vertex,
-%   Distance, Path) ordered by Vertex, and Zs is the result of deleting from Xs
-%   those elements having the same Vertex as elements in Ys.
 delete([], _, []). 
 delete([X|Xs], [], [X|Xs]):-!. 
 delete([X|Xs], [Y|Ys], Ds):-
@@ -60,10 +43,6 @@ delete([X|Xs], [Y|Ys], [X|Ds]):-
 delete([X|Xs], [_|Ys], Ds):-
   delete([X|Xs], Ys, Ds). 
   
-% merge(Xs, Ys, Zs) is true if Zs is the result of merging Xs and Ys, where Xs,
-%   Ys and Zs are lists of structures s(Vertex, Distance, Path), and are
-%   ordered by Vertex.  If an element in Xs has the same Vertex as an element
-%   in Ys, the element with the shorter Distance will be in Zs.
 merge([], Ys, Ys). 
 merge([X|Xs], [], [X|Xs]):-!. 
 merge([X|Xs], [Y|Ys], [X|Zs]):-
@@ -82,24 +61,10 @@ eq(s(X,_,_), s(X,_,_)).
 
 lt(s(X,_,_), s(Y,_,_)):-X @< Y.
 
-% incr(Xs, Incr, Ys) is true if Xs and Ys are lists of structures s(Vertex,
-%   Distance, Path), the only difference being that the value of Distance in Ys
-%   is Incr more than that in Xs.
 incr([], _, []).  
 incr([s(V,D1,P)|Xs], Incr, [s(V,D2,P)|Ys]):-
   D2 is D1 + Incr,
   incr(Xs, Incr, Ys).
-
-% member(X, Ys) is true if the element X is contained in the list Ys.
-%member(X, [X|_]).
-%member(X, [_|Ys]):-member(X, Ys).
-
-% reverse(Xs, Ys) is true if Ys is the result of reversing the order of the
-%   elements in the list Xs.
-%reverse(Xs, Ys):-reverse_1(Xs, [], Ys).
-
-%reverse_1([], As, As).
-%reverse_1([X|Xs], As, Ys):-reverse_1(Xs, [X|As], Ys).
 
 e(X, Y, Z):-node(X, Y, Z).
 e(X, Y, Z):-node(Y, X, Z).
